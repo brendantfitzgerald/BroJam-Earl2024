09bf4d08d68baacab5c569538325b880a94c2009
n8 Smith <nate.smith19@gmail.com>
2024-01-23 19:54:10 -0500

    remove unnecessary code in player script

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,6 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# I like functions for organization and we probably won't hit issues with this at the moment
